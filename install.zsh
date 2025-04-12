60af23a5c356e9ade34351c497f96e6295803f25
0xViva <0xViva@proton.me>
2025-04-12 16:15:12 +0200

    edit install to take care of DS_Store and use adapt

--- install.zsh
+++ install.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Define the path to your dotfiles directory
-DOTFILES_DIR="$HOME/dotfiles"
+DOTFILES_DIR="$HOME/.dotfiles"
 
 # Check if the dotfiles directory exists
 if [[ ! -d "$DOTFILES_DIR" ]]; then
@@ -9,13 +9,25 @@ if [[ ! -d "$DOTFILES_DIR" ]]; then
   exit 1
 fi
 
+# Clean up .DS_Store files in the dotfiles directory
+echo "Cleaning up .DS_Store files..."
+find "$DOTFILES_DIR" -name ".DS_Store" -exec rm -f {} \;
+
 # Iterate over all directories in the dotfiles directory
 for dir in "$DOTFILES_DIR"/*/; do
   # Ensure it's a directory before running stow
   if [[ -d "$dir" ]]; then
     dir_name=$(basename "$dir")
     echo "Stowing $dir_name..."
-    stow "$dir_name"
+    
+    # Use stow with --adopt to avoid conflict with existing files
+    stow --adopt "$dir_name"
+    
+    if [[ $? -ne 0 ]]; then
+      echo "Failed to stow $dir_name"
+    else
+      echo "$dir_name stowed successfully!"
+    fi
   fi
 done
 
