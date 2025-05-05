0ca28c4665e07239ada9dc9f9e136c07d1cc326b
0xViva <0xViva@proton.me>
2025-05-06 01:31:07 +0200

    remove comments

--- install.zsh
+++ install.zsh
@@ -1,26 +1,20 @@
 #!/bin/zsh
 
-# Define the path to your dotfiles directory
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# Check if the dotfiles directory exists
 if [[ ! -d "$DOTFILES_DIR" ]]; then
   echo "Dotfiles directory not found at $DOTFILES_DIR"
   exit 1
 fi
 
-# Clean up .DS_Store files in the dotfiles directory
 echo "Cleaning up .DS_Store files..."
 find "$DOTFILES_DIR" -name ".DS_Store" -exec rm -f {} \;
 
-# Iterate over all directories in the dotfiles directory
 for dir in "$DOTFILES_DIR"/*/; do
-  # Ensure it's a directory before running stow
   if [[ -d "$dir" ]]; then
     dir_name=$(basename "$dir")
     echo "Stowing $dir_name..."
     
-    # Use stow with --adopt to avoid conflict with existing files
     stow --adopt "$dir_name"
     
     if [[ $? -ne 0 ]]; then
