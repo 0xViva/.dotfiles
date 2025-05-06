7edd972c444b6b3904c3c5f67030734b37357127
0xViva <0xViva@proton.me>
2025-05-06 04:28:38 +0200

    config sketchybar

---
+++ home/.config/sketchybar/settings.lua
@@ -0,0 +1,19 @@
+return {
+	paddings = 3,
+	group_paddings = 5,
+
+	icons = "sf-symbols",
+
+	font = {
+		text = "JetBrainsMono Nerd Font", -- Used for text
+		numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+		space_numbers = "Maple Mono NF", -- Optional: This can be left if you prefer it for spacing
+		style_map = {
+			["Regular"] = "Regular",
+			["Semibold"] = "Medium",
+			["Bold"] = "SemiBold",
+			["Heavy"] = "Bold",
+			["Black"] = "ExtraBold",
+		},
+	},
+}
