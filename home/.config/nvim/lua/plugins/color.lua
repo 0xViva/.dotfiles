cbb969629b40732980c38fc45fd03fae54f9a58e
0xViva <0xViva@proton.me>
2025-05-07 17:22:06 +0200

    disable background in nvim

--- home/.config/nvim/lua/plugins/color.lua
+++ home/.config/nvim/lua/plugins/color.lua
@@ -1,89 +1,90 @@
-return 
-  {
-    'rose-pine/neovim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('rose-pine').setup {
-        variant = 'auto', -- auto, main, moon, or dawn
-        dark_variant = 'main', -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
+return {
+  'rose-pine/neovim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('rose-pine').setup {
+      variant = 'auto', -- auto, main, moon, or dawn
+      dark_variant = 'main', -- main, moon, or dawn
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
 
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
-        },
+      disable_background = true,
 
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = false,
-        },
+      enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true, -- Handle deprecated options automatically
+      },
 
-        groups = {
-          border = 'muted',
-          link = 'iris',
-          panel = 'surface',
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
 
-          error = 'love',
-          hint = 'iris',
-          info = 'foam',
-          note = 'pine',
-          todo = 'rose',
-          warn = 'gold',
+      groups = {
+        border = 'muted',
+        link = 'iris',
+        panel = 'surface',
 
-          git_add = 'foam',
-          git_change = 'rose',
-          git_delete = 'love',
-          git_dirty = 'rose',
-          git_ignore = 'muted',
-          git_merge = 'iris',
-          git_rename = 'pine',
-          git_stage = 'iris',
-          git_text = 'rose',
-          git_untracked = 'subtle',
+        error = 'love',
+        hint = 'iris',
+        info = 'foam',
+        note = 'pine',
+        todo = 'rose',
+        warn = 'gold',
 
-          h1 = 'iris',
-          h2 = 'foam',
-          h3 = 'rose',
-          h4 = 'gold',
-          h5 = 'pine',
-          h6 = 'foam',
-        },
+        git_add = 'foam',
+        git_change = 'rose',
+        git_delete = 'love',
+        git_dirty = 'rose',
+        git_ignore = 'muted',
+        git_merge = 'iris',
+        git_rename = 'pine',
+        git_stage = 'iris',
+        git_text = 'rose',
+        git_untracked = 'subtle',
 
-        palette = {
-          -- Override the builtin palette per variant
-          -- moon = {
-          --     base = '#18191a',
-          --     overlay = '#363738',
-          -- },
-        },
+        h1 = 'iris',
+        h2 = 'foam',
+        h3 = 'rose',
+        h4 = 'gold',
+        h5 = 'pine',
+        h6 = 'foam',
+      },
 
-        highlight_groups = {
-          -- Comment = { fg = "foam" },
-          -- StatusLine = { fg = "love", bg = "love", blend = 15 },
-          -- VertSplit = { fg = "muted", bg = "muted" },
-          -- Visual = { fg = "base", bg = "text", inherit = false },
-        },
+      palette = {
+        -- Override the builtin palette per variant
+        -- moon = {
+        --     base = '#18191a',
+        --     overlay = '#363738',
+        -- },
+      },
 
-        before_highlight = function(group, highlight, palette)
-          -- Disable all undercurls
-          -- if highlight.undercurl then
-          --     highlight.undercurl = false
-          -- end
-          --
-          -- Change palette colour
-          -- if highlight.fg == palette.pine then
-          --     highlight.fg = palette.foam
-          -- end
-        end,
-      }
+      highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+        -- Visual = { fg = "base", bg = "text", inherit = false },
+      },
 
-      vim.cmd 'colorscheme rose-pine'
-      -- vim.cmd("colorscheme rose-pine-main")
-      -- vim.cmd("colorscheme rose-pine-moon")
-      -- vim.cmd("colorscheme rose-pine-dawn")
-    end,
-  }
+      before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+      end,
+    }
+
+    vim.cmd 'colorscheme rose-pine'
+    -- vim.cmd("colorscheme rose-pine-main")
+    -- vim.cmd("colorscheme rose-pine-moon")
+    -- vim.cmd("colorscheme rose-pine-dawn")
+  end,
+}
