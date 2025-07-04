0e394d78cdf1d093152e9d5884eba61d74c63a28
0xViva <0xViva@proton.me>
2025-07-04 15:18:33 +0200

    set wallpapers script

---
+++ hypr/set-wallpapers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 0.5
+exec = sleep 0.5 && swww img --outputs DP-1 ~/.config/assets/backgrounds/hyprland+90de.png --transition-fps 255 --transition-type outer --transition-duration 0.8
+exec = sleep 0.5 && swww img --outputs DP-2 ~/.config/assets/backgrounds/hyprland.png --transition-fps 255 --transition-type outer --transition-duration 0.8
