8709370d58c06e7b15654e70cac2d6de127bca77
0xViva <0xViva@proton.me>
2025-07-06 12:54:26 +0200

    fix sed

--- gpg/setup.zsh
+++ gpg/setup.zsh
@@ -1,21 +1,23 @@
-
 #!/bin/zsh
+
 GPG_CONF="$HOME/.gnupg/gpg-agent.conf"
 
 mkdir -p ~/.gnupg
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     PINENTRY_PATH="/opt/homebrew/bin/pinentry-curses"
+    SED_INPLACE=("sed" "-i" "")  # BSD sed requires an empty string after -i
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     PINENTRY_PATH="/usr/bin/pinentry-curses"
+    SED_INPLACE=("sed" "-i")  # GNU sed doesn't take empty string
 else
     echo "Unsupported OS for GPG pinentry setup"
     exit 1
 fi
 
 # Update or add pinentry-program line
-if grep -q "^pinentry-program" "$GPG_CONF" 2>/dev/null; then
-    sed -i '' "s|^pinentry-program.*|pinentry-program $PINENTRY_PATH|" "$GPG_CONF"
+if [[ -f "$GPG_CONF" ]] && grep -q "^pinentry-program" "$GPG_CONF"; then
+    "${SED_INPLACE[@]}" "s|^pinentry-program.*|pinentry-program $PINENTRY_PATH|" "$GPG_CONF"
 else
     echo "pinentry-program $PINENTRY_PATH" >> "$GPG_CONF"
 fi
