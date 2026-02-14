1f786eb7c39b9571e4f0f6426b6deb754ec9923f
0xViva <0xViva@proton.me>
2026-02-14 15:51:08 +0100

    add tmux

--- stow.zsh
+++ stow.zsh
@@ -1,13 +1,18 @@
 #!/usr/bin/env zsh
 
-pushd $DOTFILES
+pushd "$DOTFILES"
 
 for folder in ${(s:,:)STOW_FOLDERS}; do
-    echo "stow $folder -> ~/.config/$folder"
-	mkdir -p "$HOME/.config/$folder"
-    stow -D -t "$HOME/.config/$folder" "$folder"
-    stow -t "$HOME/.config/$folder" "$folder"
+    if [[ "$folder" == "tmux" ]]; then
+        echo "stow $folder -> $HOME"
+        stow -D "$folder"
+        stow "$folder"
+    else
+        echo "stow $folder -> ~/.config/$folder"
+        mkdir -p "$HOME/.config/$folder"
+        stow -D -t "$HOME/.config/$folder" "$folder"
+        stow -t "$HOME/.config/$folder" "$folder"
+    fi
 done
 
 popd
-
