b36579bbc59966b95005e8be794542c0ebbb2948
0xViva <0xViva@proton.me>
2026-02-17 14:14:05 +0100

    add the possiblity to manually turn off formatting for current buffer

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,7 +14,12 @@ return { -- Autoformat
   },
   opts = {
     notify_on_error = false,
+
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       return {
         timeout_ms = 500,
         lsp_format = 'fallback',
