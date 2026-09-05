9cc0a06a95e01e3a9179cfe56d9180eecdb8abe1
0xViva <0xViva@proton.me>
2026-09-06 01:04:23 +0200

    remove provider from opencode

--- stow.zsh
+++ stow.zsh
@@ -6,16 +6,20 @@ pushd "$DOTFILES" >/dev/null || exit 1
 
 for folder in ${(s:,:)STOW_FOLDERS}; do
 
-    if [[ "$folder" == "zsh" ]]; then
-        target="$HOME"
-    elif [[ "$folder" == ".ssh" ]]; then
-        target="$HOME/.ssh"
-        mkdir -p "$target"
-        chmod 700 "$target"
-    else
-        target="$HOME/.config/$folder"
-        mkdir -p "$target"
-    fi
+    case "$folder" in
+        zsh)
+            target="$HOME"
+            ;;
+        .ssh)
+            target="$HOME/.ssh"
+            mkdir -p "$target"
+            chmod 700 "$target"
+            ;;
+        *)
+            target="$HOME/.config/$folder"
+            ;;
+    esac
+    mkdir -p "$target"
 
     echo "processing: $folder -> $target"
 
